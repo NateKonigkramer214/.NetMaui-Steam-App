39269823fee797619b6d874b8e294d1023f6743b
Kishanmaisuria <99621357+Kishanmaisuria@users.noreply.github.com>
2023-11-22 16:41:09 +1300

    fix1

---
+++ SteamInfomation/MVVM/Models/SteamGamesResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamInfomation.MVVM.Models
+{
+    public class SteamGamesResponse
+    {
+        public SteamGamesResponseData Response { get; set; }
+    }
+
+    public class SteamGamesResponseData
+    {
+        public List<SteamGame> Games { get; set; }
+    }
+}
