9ec8d9f9ba2aa53ee04aead618a2985110089b16
Kishanmaisuria <99621357+Kishanmaisuria@users.noreply.github.com>
2023-11-27 15:24:11 +1300

    Break

---
+++ SteamInfomation/MVVM/ViewModels/AchievementsViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using SteamInfomation.MVVM.Models;
+using SteamInfomation.MVVM.ViewModels;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+public class AchievementsViewModel : ViewModelBase
+{
+    private ObservableCollection<Achievement> _achievements;
+
+    public ObservableCollection<Achievement> Achievements
+    {
+        get => _achievements;
+        set
+        {
+            if (_achievements != value)
+            {
+                _achievements = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public ICommand LoadAchievementsCommand => new Command(async () => await LoadAchievements());
+
+    private async Task LoadAchievements()
+    {
+        
+        string apiUrl = " http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/";
+
+        using (HttpClient client = new HttpClient())
+        {
+            var response = await client.GetStringAsync(apiUrl);
+            var achievements = JsonConvert.DeserializeObject<List<Achievement>>(response);
+
+            Achievements = new ObservableCollection<Achievement>(achievements);
+        }
+    }
+}
